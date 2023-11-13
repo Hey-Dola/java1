a6dd4efc7accc4eb1b0a1b486095cea0131c2481
hey-dola <dolamanirohidas696@gmail.com>
2023-11-13 21:01:27 +0530

    first commit

---
+++ Program21.java
@@ -0,0 +1,13 @@
+public class Program21 {
+    // 2-dimentional array
+    public static void main(String[] args) {
+        int arr[][] = {
+            {11, 12, 13, 14},
+            {21, 22, 23},
+            {31, 32}
+        };
+        System.out.println("length of row 1 : " + arr[0].length);
+        System.out.println("length of row 2 : " + arr[1].length);
+        System.out.println("length of row 3 : " + arr[2].length);
+    }
+}
